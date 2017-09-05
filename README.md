9d3ac58b1e04f58fb905bd08901c9e8ab93382ff
Michael Noronha <michaeltnoronha@gmail.com>
2017-09-04 23:22:47 -0400

    Disable left recursion and progress on redirection

---
+++ README.md
@@ -0,0 +1,8 @@
+# pysh
+
+pysh is a small unix-style shell written in Python! It was inspired by [Destroy All Software #104](https://www.destroyallsoftware.com/screencasts/catalog/shell-from-scratch), which featured a similar but slightly smaller shell implemented in Ruby. It features both piping and (standard) redirection. :shell:
+
+## Dependencies
+
+* [TatSu](https://github.com/neogeny/TatSu)
+
--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import readline
 import tempfile
 import os
 
+
 readline.read_init_file('readline.rc')
 PROMPT = '> '
-
 
 
 def main():
@@ -24,17 +24,28 @@ def parse_line(cmdline):
     semantics = model.ModelBuilderSemantics()
     return parse(GRAMMAR,cmdline,semantics=semantics)
 
+
 def execute(tree,stdin=0):
+    print(tree)
+    # if tree['redirection']:
+    #     print('hi')
+    #     # print(tree['redirection'])
+    #     # fd = os.open()
+    #     # os.dup2(0,)
     if tree['pipeline']:
         t = tempfile.NamedTemporaryFile(mode='w')
         execute_pipeline(tree['pipeline'],t,stdin)
     else:
         execute_command(tree['command'],stdin)
 
+
 def execute_command(cmd,stdin=0,stdout=1):
+    print(cmd)
     if hasattr(cmd,'args'):
         if type(cmd.args) is contexts.closure:
+            print('hi')
             cmd = list(map(lambda x: x.unquoted_arg or x.quoted_arg[1:-1],cmd.args))
+            print(cmd)
         else:
             cmd = cmd.args.unquoted_arg or cmd.args.quoted_arg[1:-1]
         subprocess.run(cmd,stdin=stdin,stdout=stdout)
@@ -47,6 +58,7 @@ def execute_command(cmd,stdin=0,stdout=1):
             dest = os.getenv('HOME')
         os.chdir(dest)
 
+
 def execute_pipeline(pipeline,t,stdin=0):
     execute_command(pipeline.command,stdin=stdin,stdout=t)
     f = open(t.name, 'r')
@@ -56,26 +68,31 @@ def execute_pipeline(pipeline,t,stdin=0):
 
 GRAMMAR = '''
     @@grammar::CMD_PARSE
-
+    @@left_recursion :: False
 
     start = cmdline $ ;
 
-
     cmdline
         =
+        | redirection: redirection
         | pipeline: pipeline
         | command: command
+        ;
+
+    redirection
+        =
+        cmdline:cmdline '>' outfile:arg
+        ;
+
+    pipeline
+        =
+        command:command '|' cmdline:cmdline
         ;
 
     command::Command
         =
         | cd: (/cd/ | /'cd'/) [dest_dir:arg]
         | args: { arg }
-        ;
-
-    pipeline
-        =
-        command:command '|' cmdline:cmdline
         ;
 
     arg
