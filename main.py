f203b4ac572172bc52697a39b685d12c5837bb03
Michael Noronha <michaeltnoronha@gmail.com>
2017-09-06 19:35:04 -0400

    A better grammar

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ import readline
 import tempfile
 import os
 
+import json
+import pprint
 
 GRAMMAR = open('grammar.peg','r').read()
 readline.read_init_file('readline.rc')
-PROMPT = '> '
+PROMPT = '$ '
 
 
 def main():
@@ -27,14 +29,29 @@ def parse_line(cmdline):
 
 
 def execute(tree,stdin=0):
-    if tree['pipeline']:
-        if tree['pipeline']['pipeline']:
-            t = tempfile.NamedTemporaryFile(mode='w')
-            execute_pipeline(tree['pipeline'],stdin,stdout=t)
-        else:
-            execute_command(tree['pipeline']['command'],stdin)
-    if tree['cmdline']:
-        execute(tree['cmdline'])
+    print(json.dumps(tree, indent=4))
+    # if tree['pipeline']:
+    #     if tree['pipeline']['redirection']:
+    #         outfiles = tree['pipeline']['redirection']['outfile']
+    #         if type(outfiles) is list:
+    #             outfiles = list(map(lambda x: x.unquoted_arg or x.quoted_arg[1:-1],outfiles))
+    #         else:
+    #             outfiles = [outfiles.unquoted_arg or outfiles.quoted_arg[1:-1]]
+
+    #         execute_redirection(tree['pipeline']['redirection'],outfiles)
+    #         if tree['pipeline']['pipeline']:
+    #             t = tempfile.NamedTemporaryFile(mode='w')
+    #             t.write('\0')
+    #             execute_command(tree['pipeline']['pipeline']['command'],stdin=t)
+    #             t.close()
+
+    #     elif tree['pipeline']['pipeline']:
+    #         t = tempfile.NamedTemporaryFile(mode='w')
+    #         execute_pipeline(tree['pipeline'],stdin,stdout=t)
+    #     else:
+    #         execute_command(tree['pipeline']['command'],stdin)
+    # if tree['cmdline']:
+    #     execute(tree['cmdline'])
 
 
 def execute_command(cmd,stdin=0,stdout=1):
@@ -44,7 +61,7 @@ def execute_command(cmd,stdin=0,stdout=1):
         else:
             cmd = cmd.args.unquoted_arg or cmd.args.quoted_arg[1:-1]
         try:
-            subprocess.run(cmd,stdin=stdin,stdout=stdout)
+            return subprocess.run(cmd,stdin=stdin,stdout=stdout)
         except:
             print('Command failed')
     else:
@@ -56,6 +73,12 @@ def execute_command(cmd,stdin=0,stdout=1):
             dest = os.getenv('HOME')
         os.chdir(dest)
 
+def execute_redirection(cmd,outfiles):
+    cmd = execute_command(cmd.command,stdout=subprocess.PIPE)
+    if cmd.stdout:
+        for f in outfiles:
+            f = open(f,'w')
+            f.write(cmd.stdout.decode('utf-8'))
 
 def execute_pipeline(pipeline,stdin=0,stdout=1):
     execute_command(pipeline.command,stdin,stdout)
@@ -67,3 +90,4 @@ def execute_pipeline(pipeline,stdin=0,stdout=1):
 
 if __name__ =='__main__':
     main()
+
